aea321ea927b81886a8c688cc1c0f8f6467456a0
Nicolas Sorensen <nicolas.sorensen@student.uclouvain.be>
2015-04-16 18:01:21 +0200

    Added filename of the number in the structure

--- prime.c
+++ prime.c
@@ -2,26 +2,39 @@
 #include <stdio.h>
 #include <math.h>
 
-
-static int global_length = 0;	// Variable globale qui servira dans la conversion de la liste en tableau d'entiers
-								// Elle est incrementee plus bas dans prime_fact, remise a 0 dans list_to_array
-
-
+/**
+ * Ce fichier contient differentes fonctions et une structure qui gerent des nombres premiers:
+ * 
+ * - struct factor : structure qui permet de travailler avec un tableau dynamique d'entiers
+ * 	 et qui representera un facteur premier a inserer dans la liste chainee globale;
+ * - add : ajoute un element int_array dans une liste de type int_array;
+ * - is_prime : teste si le nombre en argument est un nombre premier;
+ * - next_prime : renvoie le nombre premier suivant celui en argument;
+ * - prime_fact : decompose un nombre en facteurs premiers et renvoie une liste de int_array contenant ces facteurs;
+ * 
+ **/
+ 
+ 
+ 
 
 // Structure qui represente une liste simplement chainee d'entiers. Elle servira de tableau dynamique.
-typedef struct int_tab{
+typedef struct factor{
 	int elem;
-	struct int_tab * next;
-}int_tab;
-
+	char * origin_file;
+	struct factor * next;
+}factor;
 
 
 /**
- * Ajoute @new_int a la liste d'entiers @array
+ * Ajoute un facteur premier dans la liste de facteurs du nombre courant.
  * 
- * @return : 0 si ajouté, 1 si erreur (liste vide ou element a ajouter vide)
+ * @new_int : facteur a ajouter
+ * @filename : nom du fichier dans lequel se trouvait le nombre factorise
+ * (si entree standard : filename = "stdin", si URL : filename = "<URL a lire>")
+ * @return : 0 si ajouté, 1 si erreur (liste vide)
+ * 
  */
-int add(int new_int, int_tab *array)
+int add(int new_int, char * filename, factor *array)
 {
 	
 	if(array==NULL){		//Liste vide
@@ -29,47 +42,52 @@ int add(int new_int, int_tab *array)
 	}
 	
 	else if(array->elem==-1){ 	//Premier element a inserer dans array
-		array->elem = new_int;
-		array->next = NULL;
+		array -> elem = new_int;
+		array -> origin_file = filename;
+		array -> next = NULL;
 		return 0;
 	}
 	
 	else{		//Liste non-vide
 		
-		int_tab *current = array;	// Element courant pour traverser la liste
+		factor *current = array;	// Element courant pour traverser la liste
 		while((current->next != NULL)){
 			current = current->next;
 		}
-		int_tab * element = malloc(sizeof(int_tab));
-		element -> elem = new_int;
+		factor * element = malloc(sizeof(factor));	// Creation d'un element a inserer dans la liste
+		element -> elem = new_int;		// L'element a inserer contient l'entier en argument
+		element -> origin_file = filename;	// Ajout du nom de fichier d'origine dans lequel le nombre factorise se trouvait
 		element -> next = NULL;
-		current -> next = element;
+		current -> next = element;		//Insertion de l'element
 		return 0;
-		printf("Element ajouté : %d\n", element->elem);	/// A enlever plus tard
-	}
 }
 
 
 
-
 /**
- * Teste si un nombre est premier.
+ * Teste si un nombre naturel est premier.
  * 
- * @number Le nombre qui est teste
- * @return : 1 si premier, 0 si le nombre est negatif
+ * @pre : number > 1
+ * @number : Le nombre qui est teste
+ * @return : 1 si premier, 0 si pas premier, -1 si nombre negatif en argument
  *
  */
 int is_prime(int number)
 {
 	if(number<0){
-		printf("Le nombre est negatif, il est donc considere comme non-premier.\n");
+		printf("Le nombre est negatif.\n");
+		printf("\n");
+		return -1;
+	}
+	else if(number == 1 && number == 0){
+		printf("Le nombre est egal a 0 ou 1, qui ne sont pas premiers.\n");
 		printf("\n");
 		return 0;
 	}
 	else
 	{
 		int div = 2;
-		while((double)div <= sqrt((double)number)){
+		while((double)div <= sqrt((double)number)){	// On a seulement besoin de tester jusqu'a la racine carree de number 
 			if(number % div == 0){
 				return 0;
 			}
@@ -77,19 +95,20 @@ int is_prime(int number)
 				div++;
 			}
 			else{
-				div = div + 2;
+				div = div + 2;	// Permet d'eviter de restester tous les multiples de 2 comme diviseurs
 			}
 		}
 	}
-	return 1;
+	return 1;	// Si aucun diviseur jusque racine(number) ne fonctionne, le nombre est premier.
 }
 
 
 /**
- * Renvoie le nombre premier suivant celui en argument.
+ * Renvoie le nombre premier suivant celui en argument, dans l'ordre croissant.
  * 
- * @number : le nombre duquel on part
- * @return : le nombre premier suivant @number
+ * @pre : number > 0
+ * @number : le nombre de depart
+ * @return : le nombre premier suivant @number, ou @number si celui-ci est negatif.
  */
 int next_prime(int number)
 {
@@ -100,7 +119,7 @@ int next_prime(int number)
 	}
 	else{
 		int next=number+1;
-		while(is_prime(next) !=1){
+		while(is_prime(next) !=1){	// Tant que next n'est pas premier, on l'incremente de 1.
 			next++;
 		}
 		return next;
@@ -116,43 +135,49 @@ int next_prime(int number)
  * @return : Un tableau d'entiers contenant les facteurs premiers
  * 
  */
-int_tab * prime_fact(int number)
+factor * prime_fact(int number, char * filename)
 {
+	
 	if(number <=0){
 		printf("Le nombre a factoriser est inferieur a 0\n");
 		printf("\n");
 		return NULL;
 	}
+	
 	else if(number == 1){
 		printf("1 n'est pas factorisable en facteurs premiers\n");
 		printf("\n");
 		return NULL;
 	}
+	
 	else{
-		int_tab * factors = malloc(sizeof(int_tab));	// La liste qui contiendra les facteurs premiers
+		factor * factors = malloc(sizeof(factor));	// La liste qui contiendra les facteurs premiers
 		factors -> elem = -1;	// 1er element initialise a -1 pour montrer a la fonction "add" que la liste est vide
-		int current_number = number;
+		
+		int current_number = number;	// Nombre courant
 		int div = 2;	// Diviseur courant
+		
 		int i;
 		for(i=0;current_number != 1;i++){	// La boucle s'arrete lorsqu'on arrive a 1
 			if(current_number % div ==0){	
 				current_number = current_number / div;
-				if(add(div, factors) == 1){		// Le nombre courant etait divisible par "div": on ajoute "div" comme facteur
+				if(add(div, factors, filename) == 1){		// Le nombre courant etait divisible par "div": on ajoute "div" comme facteur
 					printf("Erreur lors de l'ajout d'un facteur\n");
 					printf("\n");
 					return NULL;
 				}
-				int_tab * curr = factors;
+				/*
+				factor * curr = factors;
 				int j;
 				for(j=0;j<global_length+1;j++){		/// Simple verification de l'etat de la liste, je virerai ça plus tard
 					printf("curr2.elem = %d\n", curr->elem);
 					curr = curr->next;
 				}
 				printf("\n");
-				global_length++;		// Incrementation de la variable globale
+				*/
 			}
 			else{
-				div = next_prime(div);		// current_number pas divisible par div, on prend le prochain nombre premier
+				div = next_prime(div);	// current_number pas divisible par div, on prend le prochain nombre premier
 			}
 		}
 		return factors;
